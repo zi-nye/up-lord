308d74efef5c2ebb4329675e2c4f64365e349336
corinuk <asu970602@kakao.com>
2022-10-22 16:20:34 +0900

    인가 코드 백엔드로 전송

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Login from "./Routes/Login";
 import Router from "./Routes/router";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const onClickLoginBtn = () => {
-    setIsLoggedIn(true);
+    const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
+    const REDIRECT_URI = "http://localhost:3000/oauth";
+    const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+    window.location.href = KAKAO_AUTH_URI;
   };
+
+  useEffect(() => {
+    const code = new URL(window.location.href).searchParams.get("code");
+    console.log(code);
+  }, []);
 
   return (
     <div className="App">
