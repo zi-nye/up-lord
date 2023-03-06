e20b90af6a3f0fc7d10954a9274850357ae03f2f
corinuk <asu970602@kakao.com>
2023-03-06 22:15:48 +0900

    권한관리 퍼블리싱

--- src/Components/FixedSidebar.js
+++ src/Components/FixedSidebar.js
@@ -1,25 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 import SidebarItem from "./SidebarItem";
 
 function FixedSidebar() {
-
   const menus = [
     { name: "사용자 관리", path: "/admin_page/user" },
     { name: "권한 관리", path: "/admin_page/authority" },
     { name: "메뉴 관리", path: "/admin_page/menu" },
-    { name: "시스템 공통 코드 관리", path: "/admin_page/system-common-code" }
+    { name: "시스템 공통 코드 관리", path: "/admin_page/system-common-code" },
   ];
 
   return (
-    <div className="FixedSidebar">
+    <div>
       {menus.map((menu, index) => {
         return (
-          <Link to={menu.path} key={index}>
-            <SidebarItem
-              menu={menu}
-            />
+          <Link
+            to={menu.path}
+            key={index}
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            <SidebarItem menu={menu} />
           </Link>
         );
       })}
--- src/Routes/AdminPage.js
+++ src/Routes/AdminPage.js
@@ -1,8 +1,13 @@
 import React from "react";
 import FixedSidebar from "../Components/FixedSidebar";
+import styles from "./AdminPage.module.css";
 
 function AdminPage() {
-  return <div><FixedSidebar/></div>;
+  return (
+    <div className={styles.sidebar}>
+      <FixedSidebar />
+    </div>
+  );
 }
 
 export default AdminPage;
