c1d80fe056cbed1894ee21f084c100541ccd20e7
minsung-dev <orange@justbuild.kr>
2023-04-02 21:38:40 +0900

    권한부여기능 페이지 구현

--- src/Routes/AdminAuthority.js
+++ src/Routes/AdminAuthority.js
@@ -1,71 +1,123 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import FixedSidebar from "../Components/FixedSidebar";
 import styles from "./AdminAuthority.module.css";
+import {Api} from "../utils/Api";
 
-function AdminAuthority() {
+
+/** List **/
+function AuthorityListItem({item: {userId, userEmail, username, useYn}, onSelect}) {
   return (
-    <div>
-      <h1>권한관리</h1>
-      <div className={styles.body}>
-        <div className={styles.sidebar}>
-          <FixedSidebar />
-        </div>
-        <div className={styles.contents}>
-          <div className={styles.searching}>
-            <div className={styles.searching_}>
-              <span>계정 :&nbsp;</span>
-              <input />
-            </div>
-            <div className={styles.searching_}>
-              <span>사용자 ID :&nbsp;</span>
-              <input />
-            </div>
-            <div className={styles.searching_}>
-              <button>검색</button>
-            </div>
+      <tr>
+        <td><input onChange={onSelect} value={userId} type="checkbox"/></td>
+        <td>{userEmail}</td>
+        <td>{username}</td>
+        <td>{useYn}</td>
+      </tr>
+  );
+}
+
+function AuthorityList({onSelect}) {
+  const [authorityList, setAuthorityList] = useState([]);
+
+  useEffect(() => {
+    const getAuthorityList = async () => {
+      try {
+        const res = await Api.get('/sysManage/userManage');
+
+        console.log(res);
+        setAuthorityList(res.data);
+
+      } catch (e) {
+        //TODO 에러 바운더리?
+      }
+    };
+
+    getAuthorityList();
+  }, []);
+
+  return (
+      <div className={styles.tableDiv}>
+        <table>
+          <thead>
+          <tr>
+            <th></th>
+            <th>계정</th>
+            <th>아이디</th>
+            <th>권한</th>
+          </tr>
+          </thead>
+          <tbody>
+          {authorityList.map((item) =>
+              <AuthorityListItem key={item['userId']} item={item} onSelect={onSelect}/>
+          )}
+          </tbody>
+        </table>
+      </div>
+  )
+}
+
+
+/** PAGE **/
+function AdminAuthority() {
+  //TODO:
+  // 2. 검색
+  // 3. 페이징
+  // 4. PUT 호출
+  // 5. 권한 부여 / 권한 취소 식으로 버튼을 바꾸자.
+
+  const [userIds, setUserIds] = useState([]);
+
+  const updateUserAuthority = async (userIds, useYn) => {
+    const data = userIds.map((userId) => ({
+      userId: userId,
+      useYn: useYn,
+    }));
+
+    const res = await Api.put('/sysManage/userManage', data);
+  };
+
+
+  const onItemSelect = (e) => {
+    if(e.target.checked) {
+      setUserIds([...userIds, e.target.value]);
+    } else {
+      setUserIds(userIds.filter((id) => id !== e.target.value));
+    }
+  }
+
+  const onSaveButtonClick = async (e) => {
+    const useYn = e.target.value
+    await updateUserAuthority(userIds, useYn)
+  }
+
+
+  return (
+      <div>
+        <h1>권한관리</h1>
+        <div className={styles.body}>
+          <div className={styles.sidebar}>
+            <FixedSidebar/>
           </div>
-          <div className={styles.tableDiv}>
-            <table>
-              <th>계정</th>
-              <th>아이디</th>
-              <th>권한</th>
-              <tr>
-                <td>aaa@naver.com</td>
-                <td>abc</td>
-                <td>Y</td>
-              </tr>
-              <tr>
-                <td>bbb@naver.com</td>
-                <td>bcd</td>
-                <td>Y</td>
-              </tr>
-              <tr>
-                <td>ccc@naver.com</td>
-                <td>ccc</td>
-                <td>N</td>
-              </tr>
-            </table>
-          </div>
-          <div className={styles.saving}>
-            <div className={styles.saving_}>
-              <span>계정 :&nbsp;</span>
-              <input />
+          <div className={styles.contents}>
+            <div className={styles.searching}>
+              <div className={styles.searching_}>
+                <span>계정 :&nbsp;</span>
+                <input/>
+              </div>
+              <div className={styles.searching_}>
+                <button>검색</button>
+              </div>
             </div>
-            <div className={styles.saving_}>
-              <span>사용자 ID :&nbsp;</span>
-              <input />
-            </div>
-            <div className={styles.saving_}>
-              <span>권한</span>
-              <input type="checkbox" />
-            </div>
-            <div>
-              <button>저장</button>
+            <AuthorityList onSelect={onItemSelect}/>
+            <div className={styles.saving}>
+              <div>
+                <button onClick={onSaveButtonClick} value="Y">권한 부여</button>
+                <button onClick={onSaveButtonClick} value="N">권한 취소</button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
   );
 }
 
