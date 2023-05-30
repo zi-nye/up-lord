f1a9aa0d7baaa71981ac1aa79a239a9d90677c27
gagreen <minsang6088@naver.com>
2023-05-31 01:01:40 +0900

    Add hompage again..

---
+++ build/precache-manifest.d435a3d9516cf9bb7cf6f9359e9a034b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "403a83488a708e8af8a8",
+    "url": "/up-lord/static/css/main.a189f46f.chunk.css"
+  },
+  {
+    "revision": "403a83488a708e8af8a8",
+    "url": "/up-lord/static/js/main.403a8348.chunk.js"
+  },
+  {
+    "revision": "b5556b5337580411b825",
+    "url": "/up-lord/static/css/1.896edd4b.chunk.css"
+  },
+  {
+    "revision": "b5556b5337580411b825",
+    "url": "/up-lord/static/js/1.b5556b53.chunk.js"
+  },
+  {
+    "revision": "e60de32f5e452837486d",
+    "url": "/up-lord/static/js/runtime~main.e60de32f.js"
+  },
+  {
+    "revision": "30e319db1928f1356be9dd6ab162c27e",
+    "url": "/up-lord/static/media/logo1.30e319db.png"
+  },
+  {
+    "revision": "7ae4ccfba3a6e6609a80df9d64d3c07a",
+    "url": "/up-lord/index.html"
+  }
+];
