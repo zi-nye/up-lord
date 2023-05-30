fdd605a6c7004b8f928bdaa49c561a5c0444b1a6
gagreen <minsang6088@naver.com>
2023-05-31 00:58:32 +0900

    Add hompage again..

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ace1576df88265ce79c4473110e9f6f4.js"
+  "/up-lord/precache-manifest.d435a3d9516cf9bb7cf6f9359e9a034b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/up-lord/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
