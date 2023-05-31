bd893bf9ce0ce6ca75f908804ae857e5af0d1f64
gagreen <minsang6088@naver.com>
2023-06-01 00:38:40 +0900

    clean code & 배포 설정 수정

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter} from "react-router-dom";
 
+console.log(`아니 왜 안되냐고.. ${process.env.PUBLIC_URL}`);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
