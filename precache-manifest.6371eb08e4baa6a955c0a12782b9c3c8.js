c7230ba6dda398926ba3d5e8a11df86b24404c6a
gagreen <gagreen@users.noreply.github.com>
2023-05-31 16:07:15 +0000

    deploy: f90a8166e6301349aa93e633ccb34df2753b2f20

---
+++ precache-manifest.6371eb08e4baa6a955c0a12782b9c3c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4332e2ef1189755a97a",
+    "url": "/up-lord/static/css/main.21cd1086.chunk.css"
+  },
+  {
+    "revision": "c4332e2ef1189755a97a",
+    "url": "/up-lord/static/js/main.c4332e2e.chunk.js"
+  },
+  {
+    "revision": "cea5c645b74c43fa990d",
+    "url": "/up-lord/static/css/1.07f0502d.chunk.css"
+  },
+  {
+    "revision": "cea5c645b74c43fa990d",
+    "url": "/up-lord/static/js/1.cea5c645.chunk.js"
+  },
+  {
+    "revision": "e60de32f5e452837486d",
+    "url": "/up-lord/static/js/runtime~main.e60de32f.js"
+  },
+  {
+    "revision": "30e319db1928f1356be9dd6ab162c27e",
+    "url": "/up-lord/static/media/logo1.30e319db.png"
+  },
+  {
+    "revision": "dd014e3d8b2f9525d0f898aad1c01da2",
+    "url": "/up-lord/index.html"
+  }
+];
